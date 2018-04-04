8694bde246f6723a52479c0511ed9bf865be1761
Kuanysh <kuanyshoteu@gmail.com>
2018-04-05 01:34:57 +0600

    Problem view updates

---
+++ Enigmath/lemmas/codes.py
@@ -0,0 +1,26 @@
+from sympy import *
+from lemmas.split import splitting
+class LemmaCode(object):
+    def isequal(input_exp):
+        status = False
+        newStr = str(input_exp)
+
+        RHS = ""
+        LHS = ""
+        znak = ""
+
+        bool = False
+        splitted_array = splitting(newStr)
+        if splitted_array == 'Wrong':
+            print('de')
+            return splitted_array
+        LHS = splitted_array[0]
+        RHS = splitted_array[1]
+        znak = splitted_array[2]
+        result = 'Wrong'
+        if LHS.expand() == RHS.expand() and znak == '=':
+            status = True
+        if status == True:
+            result = 'Correct'
+        return result
+    
